16a58644b4853eec0843e86e3c53cdb7fb8dc3ca
tamimi80 <77532825+tamimi80@users.noreply.github.com>
2021-01-16 13:45:54 +0200

    تم التحميل

---
+++ qstat.js
@@ -0,0 +1,296 @@
+var questions = [{
+    question: " س 1 :- من مقاييس النزعة المركزية الذي يمكن ان يكون له اكثر من قيمة هو  ؟",
+     choices: ["الوسط الحسابي", "المنوال", "الوسيط", "التباين"],
+    correctAnswer: 1
+}, {
+    question: " س 2 :- من مقاييس النزعة المركزية الذي يمكن ان  لا يوجد في البيانات   ؟ ",
+    choices: ["الوسط الحسابي", "المنوال", "الوسيط", "التباين"],
+    correctAnswer: 1
+}, {
+    question: "  س 3 :- اذا كانت البيانات كالتالي , 9 , 12, 13, 9 , 5 , 7 , 15  فان الوسط الحسابي = ؟ ",
+    choices: ["9", "14", "7", "70"],
+    correctAnswer: 1
+}, 
+ {
+    question: "  س 4:- اذا كانت البيانات كالتالي , 9 , 12, 13, 9 , 5 , 7 , 15  فان الوسيط  = ؟ ",
+    choices: ["9", "7", "4", "12"],
+    correctAnswer: 3
+}, 
+{
+    question: "  س 4:- اذا كانت البيانات كالتالي , 9 , 12, 13, 9 , 5 , 7 , 15  فان  رتبة الوسيط  = ؟ ",
+    choices: ["9", "14", "7", "4"],
+    correctAnswer: 3
+}, 
+ {
+    question: "  س 5:- اذا كانت البيانات كالتالي , 9 , 12, 13, 9 , 5 , 7 , 15  فان المنوال هو  = ؟ ",
+    choices: ["9", "14", "7", "70"],
+    correctAnswer: 0
+}, 
+];
+
+
+var currentQuestion = 0;
+var viewingAns = 0;
+var correctAnswers = 0;
+var quizOver = false;
+var iSelectedAnswer = [];
+	var c=180;
+	var t;
+$(document).ready(function () 
+{
+    // Display the first question
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+    $(this).find(".preButton").attr('disabled', 'disabled');
+	
+	timedCount();
+	
+	$(this).find(".preButton").on("click", function () 
+	{		
+		
+        if (!quizOver) 
+		{
+			if(currentQuestion == 0) { return false; }
+	
+			if(currentQuestion == 1) {
+			  $(".preButton").attr('disabled', 'disabled');
+			}
+			
+				currentQuestion--; // Since we have already displayed the first question on DOM ready
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					
+				} 					
+		} else {
+			if(viewingAns == 3) { return false; }
+			currentQuestion = 0; viewingAns = 3;
+			viewResults();		
+		}
+    });
+
+	
+	// On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () 
+	{
+        if (!quizOver) 
+		{
+			
+            var val = $("input[type='radio']:checked").val();
+
+            if (val == undefined) 
+			{
+                $(document).find(".quizMessage").text("الرجاء اختيار الاجابة الصحيحة");
+                $(document).find(".quizMessage").show();
+            } 
+			else 
+			{
+                // TODO: Remove any message -> not sure if this is efficient to call this each time....
+                $(document).find(".quizMessage").hide();
+				if (val == questions[currentQuestion].correctAnswer) 
+				{
+					correctAnswers++;
+				}
+				iSelectedAnswer[currentQuestion] = val;
+				
+				currentQuestion++; // Since we have already displayed the first question on DOM ready
+				if(currentQuestion >= 1) {
+					  $('.preButton').prop("disabled", false);
+				}
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					
+				} 
+				else 
+				{
+					displayScore();
+					$('#iTimeShow').html('انتهى وقت الاختبار !');
+					$('#timer').html("نتيجتك هي : " + correctAnswers + " من  " + questions.length);
+					c=185;
+					$(document).find(".preButton").text("اظهار الاجابات");
+					// $(document).find(".nextButton").text("حاول مرة اخرى");
+					 //quizOver = true;
+					return false;
+					
+				}
+			}
+					
+		}	
+		else 
+		{ // quiz is over and clicked the next button (which now displays 'Play Again?'
+			quizOver = false; $('#iTimeShow').html('الوقت المتبقي '); iSelectedAnswer = [];
+			$(document).find(".nextButton").text("السؤال التالي");
+			$(document).find(".preButton").text("السؤال السابق");
+			 $(".preButton").attr('disabled', 'disabled');
+			resetQuiz();
+			viewingAns = 1;
+			displayCurrentQuestion();
+			hideScore();
+		}
+    });
+});
+
+
+
+function timedCount()
+	{
+		if(c == 185) 
+		{ 
+			return false; 
+		}
+		
+		var hours = parseInt( c / 3600 ) % 24;
+		var minutes = parseInt( c / 60 ) % 60;
+		var seconds = c % 60;
+		var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);            
+		$('#timer').html(result);
+		
+		if(c == 0 )
+		{
+					displayScore();
+					$('#iTimeShow').html('انتهى وقت الاختبار');
+					$('#timer').html("نتيجتك " + correctAnswers + " من  " + questions.length);
+					c=185;
+					$(document).find(".preButton").text("اظهار الاجابات ");
+					// $(document).find(".nextButton").text("حاول مرة اخرى");
+					// quizOver = true;
+					return false;
+					
+		}
+		
+		/*if(c == 0 )
+		{	
+			if (!quizOver) 
+			{
+				var val = $("input[type='radio']:checked").val();
+            	if (val == questions[currentQuestion].correctAnswer) 
+				{
+					correctAnswers++;
+				}
+				currentQuestion++; // Since we have already displayed the first question on DOM ready
+				
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					c=15;
+				} 
+				else 
+				{
+					displayScore();
+					$('#timer').html('');
+					c=16;
+					$(document).find(".nextButton").text("حاول مرة اخرى?");
+					quizOver = true;
+					return false;
+				}
+			}
+			else 
+			{ // quiz is over and clicked the next button (which now displays 'Play Again?'
+				quizOver = false;
+				$(document).find(".nextButton").text("Next Question");
+				resetQuiz();
+				displayCurrentQuestion();
+				hideScore();
+			}		
+		}	*/
+		c = c - 1;
+		t = setTimeout(function()
+		{
+			timedCount()
+		},1000);
+	}
+	
+	
+// This displays the current question AND the choices
+function displayCurrentQuestion() 
+{
+
+	if(c == 185) { c = 180; timedCount(); }
+    //console.log("In display current Question");
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+    var choice;
+	
+	
+    for (i = 0; i < numChoices; i++) 
+	{
+        choice = questions[currentQuestion].choices[i];
+		
+		if(iSelectedAnswer[currentQuestion] == i) {
+			$('<li><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+		} else {
+			$('<li><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+		}
+    }
+}
+
+function resetQuiz()
+{
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore()
+{
+    $(document).find(".quizContainer > .result").text("نتيجتك: " + correctAnswers + "من : " + questions.length);
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() 
+{
+    $(document).find(".result").hide();
+}
+
+// This displays the current question AND the choices
+function viewResults() 
+{
+
+	if(currentQuestion == 10) { currentQuestion = 0;return false; }
+	if(viewingAns == 1) { return false; }
+
+	hideScore();
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+    var choice;
+	
+	
+	for (i = 0; i < numChoices; i++) 
+	{
+        choice = questions[currentQuestion].choices[i];
+		
+		if(iSelectedAnswer[currentQuestion] == i) {
+			if(questions[currentQuestion].correctAnswer == i) {
+				$('<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			} else {
+				$('<li style="border:2px solid red;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			}
+		} else {
+			if(questions[currentQuestion].correctAnswer == i) {
+				$('<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			} else {
+				$('<li><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			}
+		}
+    }
+	
+	currentQuestion++;
+	
+	setTimeout(function()
+		{
+			viewResults();
+		},3000);
+}
